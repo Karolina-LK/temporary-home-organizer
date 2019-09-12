11ea519e3fabfc10bf5c08f217e87c1edf4cc09d
Krzychu <42058392+kmochocki@users.noreply.github.com>
2019-09-12 20:42:37 +0200

    Remove adresses

--- src/main/java/tasks/jigsaw/UrlResolver.java
+++ src/main/java/tasks/jigsaw/UrlResolver.java
@@ -2,10 +2,10 @@ package tasks.jigsaw;
 
 public interface UrlResolver {
 
-  String PERFORM_DOMAIN = "performgroup.com";
-  String DAZN_DOMAIN = "daznservices.com";
-  String DEFAULT_IMAGE_DOMAIN = "images.performgroup.com";
-  String DAZN_IMAGE_DOMAIN = "images.daznservices.com";
+  String A_DOMAIN = "a.com";
+  String B_DOMAIN = "b.com";
+  String DEFAULT_IMAGE_A = "images.a.com";
+  String B_IMAGE_DOMAIN = "images.b.com";
   String HTTP_URI_PROTOCOL_SCHEMA = "http:";
   String HTTPS_URI_PROTOCOL_SCHEMA = "https:";
 
