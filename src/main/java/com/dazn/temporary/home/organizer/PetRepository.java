e146c09e3a6f963debed36fd8832780713d74dc3
Karolina Lubieniecka-Kocon <lubieniecka@interia.pl>
2019-10-18 05:05:55 +0200

    Make PetRepository Singleton-ish

--- src/main/java/com/dazn/temporary/home/organizer/PetRepository.java
+++ src/main/java/com/dazn/temporary/home/organizer/PetRepository.java
@@ -4,7 +4,37 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-public class PetRepository {
+public final class PetRepository {
+
+  //lazy instantiating - to add set-as-null class reference
+  private static PetRepository petRepositoryInstance = null;
+
+  //setting constructor to private in order to prevent illegal creating the class object
+  private PetRepository() {
+
+  }
+
+  //Singleton class of getInstance
+  public static PetRepository getInstance() {
+
+    if (petRepositoryInstance == null) {
+      petRepositoryInstance = new PetRepository();
+    }
+    return petRepositoryInstance;
+  }
+
+  //other version found to prevent multi-thread issue problems
+//  public static PetRepository getInstance() {
+//
+//    if (petRepositoryInstance == null) {
+//      synchronized (PetRepository.class) {
+//        if (petRepositoryInstance == null) {
+//          petRepositoryInstance = new PetRepository();
+//        }
+//      }
+//    }
+//    return petRepositoryInstance;
+//  }
 
   private static List<PetLookingForHome> pets = Arrays.asList(
       new PetLookingForHome(1, "Arnold", "M", 4.5, true, false, true),
